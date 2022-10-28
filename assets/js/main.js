676a1677cb5979494eef6b0127c4ab4f2a5ab761
sidahmed2306 <sidahmedbc1@gmail.com>
2022-10-28 17:54:50 +0200

    add

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+const brutto = document.getElementById("bruttorechner");
+const netto = document.getElementById("nettorechner");
+const prozent19 = document.getElementById("prozen19");
+const prozent7 = document.getElementById("prozent7");
+const inputbetarg = document.getElementById("input");
+const h2 = document.getElementById("netto-brutto");
+const mehrwertsteuerBetrag = document.getElementById("betrag");
+const bruttoNetto = document.getElementById("bruttoNettoEndBetrag");
+const result = document.getElementById("result");
+
+function bruttoZuNetto(event) {
+  event.preventDefault();
+  if (brutto.checked == true) {
+    h2.innerHTML = `Nettobetrag (preis ohne Mehrwertsteuer) in Euro`;
+    bruttoNetto.innerHTML = "Bruttobetrrag";
+  } else if (netto.checked == true) {
+    h2.innerHTML = "Bruttobetrag (preis inklusive Mehrwertsteuer) in Euro";
+    bruttoNetto.innerHTML = "Nettobetrag";
+  }
+}
+
+function mehrertsteurRechner(event) {
+  event.preventDefault();
+  console.log(brutto.checked);
+  if (brutto.checked == true && prozent19.checked == true) {
+    mehrwertsteuerBetrag.innerHTML =
+      ((Number(inputbetarg.value) * 19) / 100).toFixed(2) + " €";
+
+    result.innerHTML = (inputbetarg.value * 1.19).toFixed(2) + " €";
+  } else if (brutto.checked == true && prozent7.checked == true) {
+    mehrwertsteuerBetrag.innerHTML =
+      ((Number(inputbetarg.value) * 7) / 100).toFixed(2) + " €";
+    result.innerHTML = (inputbetarg.value * 1.07).toFixed(2) + " €";
+  } else if (netto.checked == true && prozent19.checked == true) {
+    mehrwertsteuerBetrag.innerHTML =
+      ((Number(inputbetarg.value) / 1.19) * 0.19).toFixed(2) + " €";
+    result.innerHTML = (Number(inputbetarg.value) / 1.19).toFixed(2) + " €";
+  } else if (netto.checked == true && prozent7.checked == true) {
+    mehrwertsteuerBetrag.innerHTML =
+      ((Number(inputbetarg.value) / 1.07) * 0.07).toFixed(2) + " €";
+    result.innerHTML = (Number(inputbetarg.value) / 1.07).toFixed(2) + " €";
+  }
+}
